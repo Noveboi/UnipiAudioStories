d92da90abda31fc212fc1dbebe7953d04a254f06
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-01 18:00:16 +0200

    Add javadoc and add 'Story Service' (#11)

---
+++ app/src/main/java/com/example/unipiaudiostories/core/StoryService.java
@@ -0,0 +1,16 @@
+package com.example.unipiaudiostories.core;
+
+import com.example.unipiaudiostories.domain.Story;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Provides methods that cover the 'story' use-cases.
+ */
+public class StoryService {
+    public List<Story> GetAllStories() {
+        // TODO!
+        return new ArrayList<>();
+    }
+}
