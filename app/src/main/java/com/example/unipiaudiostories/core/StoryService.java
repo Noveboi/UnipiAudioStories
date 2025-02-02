13ad2010d505e841841592699c5c0f2134ed3ed7
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-02 23:00:52 +0200

    Moved firebase logic in StoryService

--- app/src/main/java/com/example/unipiaudiostories/core/StoryService.java
+++ app/src/main/java/com/example/unipiaudiostories/core/StoryService.java
@@ -1,6 +1,8 @@
 package com.example.unipiaudiostories.core;
 
 import com.example.unipiaudiostories.domain.Story;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +11,15 @@ import java.util.List;
  * Provides methods that cover the 'story' use-cases.
  */
 public class StoryService {
+
+    FirebaseDatabase database;
+    DatabaseReference storiesReference;
+
+    public StoryService() {
+        database = FirebaseDatabase.getInstance();
+        storiesReference = database.getReference("stories");
+    }
+
     public List<Story> getAllStories() {
         // TODO!
         return new ArrayList<>();
