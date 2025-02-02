bd0afb1cd7de2944eb44e7a5b6ce0e943cdc99f1
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-02-02 19:51:40 +0200

    Completed firebaseDb setup and added simple MainActivity logic to upload stories in the db (closes #3)

--- app/src/main/java/com/example/unipiaudiostories/ui/MainActivity.java
+++ app/src/main/java/com/example/unipiaudiostories/ui/MainActivity.java
@@ -10,10 +10,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.unipiaudiostories.R;
 import com.example.unipiaudiostories.databinding.ActivityMainBinding;
+import com.example.unipiaudiostories.domain.Story;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+
+    FirebaseDatabase database;
+    DatabaseReference storiesReference;
+
+    // This is a test. When we get the actual stories, we will add them in the database in this way!
+    Story story1 = new Story(UUID.randomUUID(), "test", "test", "test", 2000, 1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +39,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        database = FirebaseDatabase.getInstance();
+        storiesReference = database.getReference("stories");
+
+        this.writeStoryToDb(story1);
+    }
+
+    public void writeStoryToDb(Story story){
+        storiesReference.child("story1").setValue(story);
     }
 }
