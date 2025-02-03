d36a57d2aade57b64414e067f1541ea847ebd34d
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-03 11:08:48 +0200

    Intermediate commit

--- app/src/main/java/com/example/unipiaudiostories/core/SettingsService.java
+++ app/src/main/java/com/example/unipiaudiostories/core/SettingsService.java
@@ -1,4 +1,19 @@
 package com.example.unipiaudiostories.core;
 
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+
+import java.util.Locale;
+
 public class SettingsService {
+    public static void setLanguage(Context context, String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+
+        Resources resources = context.getResources();
+        Configuration configuration = resources.getConfiguration();
+
+        configuration.setLocale(locale);
+    }
 }
--- app/src/main/java/com/example/unipiaudiostories/core/StoryService.java
+++ app/src/main/java/com/example/unipiaudiostories/core/StoryService.java
@@ -1,5 +1,6 @@
 package com.example.unipiaudiostories.core;
 
+import com.example.unipiaudiostories.R;
 import com.example.unipiaudiostories.domain.Story;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,6 +19,10 @@ public class StoryService {
     public StoryService() {
         database = FirebaseDatabase.getInstance();
         storiesReference = database.getReference("stories");
+
+        // Add stories if they don't exist
+        List<Story> stories = new ArrayList<>();
+        stories.add(new Story(1, "Example", "Test", "Mary", 2025, R.drawable.cat));
     }
 
     public List<Story> getAllStories() {
