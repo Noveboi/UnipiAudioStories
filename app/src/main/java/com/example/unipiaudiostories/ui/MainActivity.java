57af4a9f35b0c5e32d17809d94894d09ee563159
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-03 13:09:20 +0200

    Setup RecyclerView adapter to use async callback

--- app/src/main/java/com/example/unipiaudiostories/ui/MainActivity.java
+++ app/src/main/java/com/example/unipiaudiostories/ui/MainActivity.java
@@ -3,26 +3,26 @@ package com.example.unipiaudiostories.ui;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.unipiaudiostories.R;
 import com.example.unipiaudiostories.core.StoryService;
 import com.example.unipiaudiostories.databinding.ActivityMainBinding;
-import com.example.unipiaudiostories.domain.Story;
 import com.example.unipiaudiostories.ui.list.StoryRecyclerViewAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppActivityBase<ActivityMainBinding> {
     @Override
     protected void onAfterCreate() {
         RecyclerView storiesList = getBinding().rvStories;
-        List<Story> stories = new ArrayList<>();
-        stories.add(new Story(1, "Bugs", "ok", "Mary", 2024, R.drawable.bugs));
-        stories.add(new Story(2, "Cat", "Ok", "Mary", 2025, R.drawable.cat));
-        stories.add(new Story(3, "Dog Cake", "Ok", "Mary", 2025, R.drawable.birthday_dog));
 
-        StoryRecyclerViewAdapter adapter = new StoryRecyclerViewAdapter(stories);
+        StoryRecyclerViewAdapter adapter = new StoryRecyclerViewAdapter();
         storiesList.setAdapter(adapter);
         storiesList.setLayoutManager(new GridLayoutManager(this, 2));
+
+        StoryService storyService = new StoryService();
+
+        storyService.getAllStories(stories -> {
+            if (stories == null)
+                return;
+
+            adapter.setStories(stories);
+        });
     }
 }
