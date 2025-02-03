5e802080aaaa652d531c86ce85d8ea912c0d8347
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-03 12:19:10 +0200

    Make all activities implement AppActivityBase

--- app/src/main/java/com/example/unipiaudiostories/ui/SettingsActivity.java
+++ app/src/main/java/com/example/unipiaudiostories/ui/SettingsActivity.java
@@ -1,32 +1,10 @@
 package com.example.unipiaudiostories.ui;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.example.unipiaudiostories.R;
 import com.example.unipiaudiostories.databinding.ActivitySettingsBinding;
 
-public class SettingsActivity extends AppCompatActivity {
-
-    ActivitySettingsBinding binding;
-
+public class SettingsActivity extends AppActivityBase<ActivitySettingsBinding> {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+    protected void onAfterCreate() {
 
-        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
--- app/src/main/java/com/example/unipiaudiostories/ui/StatisticsActivity.java
+++ app/src/main/java/com/example/unipiaudiostories/ui/StatisticsActivity.java
@@ -1,32 +1,11 @@
 package com.example.unipiaudiostories.ui;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.example.unipiaudiostories.R;
 import com.example.unipiaudiostories.databinding.ActivityStatisticsBinding;
 
-public class StatisticsActivity extends AppCompatActivity {
-
-    ActivityStatisticsBinding binding;
+public class StatisticsActivity extends AppActivityBase<ActivityStatisticsBinding> {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+    protected void onAfterCreate() {
 
-        binding = ActivityStatisticsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
