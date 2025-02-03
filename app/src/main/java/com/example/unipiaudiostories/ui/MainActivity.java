85edff5cf638bd2f0c24e4635f8a80a87c131e3f
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-03 13:24:12 +0200

    Add navigation intent in MainActivity

--- app/src/main/java/com/example/unipiaudiostories/ui/MainActivity.java
+++ app/src/main/java/com/example/unipiaudiostories/ui/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.unipiaudiostories.ui;
 
+import android.content.Intent;
+
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.unipiaudiostories.core.StoryService;
 import com.example.unipiaudiostories.databinding.ActivityMainBinding;
+import com.example.unipiaudiostories.domain.Story;
 import com.example.unipiaudiostories.ui.list.StoryRecyclerViewAdapter;
 
 public class MainActivity extends AppActivityBase<ActivityMainBinding> {
@@ -12,7 +15,7 @@ public class MainActivity extends AppActivityBase<ActivityMainBinding> {
     protected void onAfterCreate() {
         RecyclerView storiesList = getBinding().rvStories;
 
-        StoryRecyclerViewAdapter adapter = new StoryRecyclerViewAdapter(s -> {});
+        StoryRecyclerViewAdapter adapter = new StoryRecyclerViewAdapter(this::navigateToStory);
         storiesList.setAdapter(adapter);
         storiesList.setLayoutManager(new GridLayoutManager(this, 2));
 
@@ -24,5 +27,12 @@ public class MainActivity extends AppActivityBase<ActivityMainBinding> {
 
             adapter.setStories(stories);
         });
+    }
+
+    private void navigateToStory(Story story) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra("story_id", story.getId());
+
+        startActivity(intent);
     }
 }
