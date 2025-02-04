739f429a86448587d850fbeb450de9427b1c0c8a
George Nikolaidis <cptgiorgos03@gmail.com>
2025-02-04 21:09:14 +0200

    Added item spacing to story RecyclerView as well as variable columns based on screen width

--- app/src/main/java/com/example/unipiaudiostories/ui/MainActivity.java
+++ app/src/main/java/com/example/unipiaudiostories/ui/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.unipiaudiostories.ui;
 
 import android.content.Intent;
 
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.unipiaudiostories.core.StoryService;
@@ -11,19 +9,18 @@ import com.example.unipiaudiostories.databinding.ActivityMainBinding;
 import com.example.unipiaudiostories.domain.Story;
 import com.example.unipiaudiostories.ui.constants.ActivityIntents;
 import com.example.unipiaudiostories.ui.list.StoryRecyclerViewAdapter;
+import com.example.unipiaudiostories.ui.list.StoryRecyclerViewHelper;
 
 public class MainActivity extends AppActivityBase<ActivityMainBinding> {
     @Override
     protected void onAfterCreate() {
         RecyclerView storiesList = getBinding().rvStories;
+        StoryRecyclerViewHelper.applyStyling(this, storiesList);
 
         StoryRecyclerViewAdapter adapter = new StoryRecyclerViewAdapter(this::navigateToStory);
-        storiesList.setLayoutManager(new GridLayoutManager(this, 2));
-        storiesList.setItemAnimator(new DefaultItemAnimator());
         storiesList.setAdapter(adapter);
 
         StoryService storyService = new StoryService();
-
         storyService.getAllStories(stories -> {
             if (stories == null)
                 return;
---
+++ app/src/main/java/com/example/unipiaudiostories/ui/list/StoryRecyclerViewHelper.java
@@ -0,0 +1,25 @@
+package com.example.unipiaudiostories.ui.list;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.unipiaudiostories.R;
+
+public class StoryRecyclerViewHelper {
+    public static void applyStyling(Context context, RecyclerView storiesList) {
+        Resources resources = context.getResources();
+
+        float columnWidth = resources.getDimension(R.dimen.story_item_width);
+        int itemSpacing = 15;
+        float itemSize = columnWidth + itemSpacing + 10;
+        int spanCount = (int) Math.max(2.0f, resources.getDisplayMetrics().widthPixels / itemSize);
+
+        storiesList.setLayoutManager(new GridLayoutManager(context, spanCount));
+        storiesList.setItemAnimator(new DefaultItemAnimator());
+        storiesList.addItemDecoration(new StoryItemDecoration(itemSpacing));
+    }
+}
